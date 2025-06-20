b6c43d10ac165bc3bd73978ae9643432215963f6
Dmytro Vasylchuk <dmytro.vasylchuk@gabsoft.io>
2025-06-20 19:00:56 +0300

    feat: add phone

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,6 +41,19 @@
                         </div>
 
                         <div class="input-group mb-3">
+                            <input type="tel" name="phone_number" value="{{ old('phone_number') }}"
+                                class="form-control @error('phone_number') is-invalid @enderror" placeholder="Phone">
+                            <div class="input-group-append">
+                                <div class="input-group-text"><span class="fas fa-phone"></span></div>
+                            </div>
+                            @error('phone_number')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
+                        </div>
+
+                        <div class="input-group mb-3">
                             <input type="password" name="password"
                                 class="form-control @error('password') is-invalid @enderror" placeholder="Password">
                             <div class="input-group-append">
